1b323aa1b9bf5abf727be02c0543e0f36dcf37aa
kan5016@naver.com <kan5016@naver.com>
2022-02-17 22:49:41 +0900

    fix: progressRing 오류 수정 - progressRing backgroundColor, progressColor 가 변경되지 않는 버그 해결 - progressRing 이 세로로 길어졌을 때 원이 view의 중앙에 오지 않는 버그 해결

--- Timer/src/main/java/com/partnus/timer/front/TimerProgressRing.kt
+++ Timer/src/main/java/com/partnus/timer/front/TimerProgressRing.kt
@@ -2,11 +2,12 @@ package com.partnus.timer.front
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.RotateDrawable
+import android.os.Build
 import android.util.AttributeSet
-import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import com.partnus.timer.R
 
 class TimerProgressRing(context: Context, attrs: AttributeSet) :
@@ -27,11 +28,11 @@ class TimerProgressRing(context: Context, attrs: AttributeSet) :
         setInitTime(100) // default initTime
         setCurrentTime(0) // default current
 
-        layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, 0)
+        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
 
-        progressRing = findViewById<ProgressBar>(R.id.progressRing).apply {
-            context.obtainStyledAttributes(attrs, R.styleable.Timer).also { typedArray ->
-                // Todo : custom drawable 을 변경하는 방법은 없을까?
+        progressRing = findViewById(R.id.progressRing)
+        context.obtainStyledAttributes(attrs, R.styleable.Timer).also { typedArray ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 // set progress background color
                 setProgressBackgroundColor(
                     typedArray.getInt(
@@ -49,36 +50,43 @@ class TimerProgressRing(context: Context, attrs: AttributeSet) :
                 )
             }
         }
-
         invalidateProgress()
     }
 
     private fun invalidateProgress() {
         // currentTime, initTime 기반으로 progress 상태 업데이트
-        progressRing?.progress = (TimerProgressBar.currentTime * 100 / TimerProgressBar.initTime.toDouble()).toInt()
+        progressRing?.progress = (currentTime * 100 / initTime.toDouble()).toInt()
     }
 
-    override fun getCurrentTime(): Long = TimerProgressBar.currentTime
+    override fun getCurrentTime(): Long = currentTime
 
-    override fun getInitTime(): Long = TimerProgressBar.initTime
+    override fun getInitTime(): Long = initTime
 
     override fun setCurrentTime(timestamp: Long) {
-        TimerProgressBar.currentTime = timestamp
+        currentTime = timestamp
         invalidateProgress()
     }
 
     override fun setInitTime(timestamp: Long) {
-        TimerProgressBar.initTime = timestamp
+        initTime = timestamp
         invalidateProgress()
     }
 
     override fun setProgressBackgroundColor(color: Int) {
-        // Todo : custom drawable 을 변경하는 방법은 없을까?
-        progressRing?.progressBackgroundTintList = ColorStateList.valueOf(color)
+        progressRing?.background?.also { drawable ->
+            val shapeDrawable = drawable as GradientDrawable
+            shapeDrawable.setColor(color)
+        }
     }
 
     override fun setProgressColor(color: Int) {
-        // Todo : custom drawable 을 변경하는 방법은 없을까?
-        progressRing?.progressTintList = ColorStateList.valueOf(color)
+        progressRing?.progressDrawable?.also { drawable ->
+            val shapeDrawable = drawable as RotateDrawable
+            shapeDrawable.setTintList(ColorStateList.valueOf(color))
+        }
+    }
+
+    override fun setProgressTextSize(size: Float) {
+        // progress ring 은 현재 text가 없지만, text 로 표현하는 기능이 추가될 것을 대비하여 생성
     }
 }
