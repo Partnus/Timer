05891a868c1d08966e2acff9a81e8bcab89ccbae
nosorae <nosorae@naver.com>
2022-02-27 13:36:46 +0900

    Feat(TimerWorker.kt, MainActivity.kt): PostDelayed 에서 sleep 으로 변경, 테스트 완료 (265ms 오차 확인)

--- Timer/src/main/java/com/partnus/timer/back/TimerWorker.kt
+++ Timer/src/main/java/com/partnus/timer/back/TimerWorker.kt
@@ -3,27 +3,34 @@ package com.partnus.timer.back
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
-import androidx.core.os.postDelayed
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.lang.Thread.sleep
 
-class TimerWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+class TimerWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
     override fun doWork(): Result {
         try {
-            inputData.getString(TimerHandler.WORKER_DATA_PARAM_TIME)?.toLong()?.let { ms ->
-                showToastDelayed(ms) // TODO Toast 를 Notification 코드로 바꾸기
+            inputData.getLong(TimerHandler.WORKER_DATA_PARAM_TIME, 0).also { ms ->
+                sleep(ms)
+                Log.e("TAG_TIMER_THREAD", "타이머 끝")
+                showToastDelayed() // TODO Notification 으로 교체
             }
             return Result.success()
         } catch (e: Exception) {
             return Result.failure()
         }
-
-
     }
-    private fun showToastDelayed(ms: Long) {
-        Handler(Looper.getMainLooper()).postDelayed(ms) {
+
+    private fun showToastDelayed() {
+        Handler(Looper.getMainLooper()).post() {
             Toast.makeText(applicationContext, "TimerWorker 호출됨", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun createNotification() {
+
     }
 }
--- app/src/main/java/com/partnus/timer/MainActivity.kt
+++ app/src/main/java/com/partnus/timer/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.partnus.timer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.partnus.timer.back.TimerThread
 import com.partnus.timer.front.Timer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testProgressRing()
+        testTimerWorkerRequest()
+    }
+
+    private fun testProgressRing() {
         // progressbar 생성 및 테스트 Code
         // Todo : background 기능구현 적용하기 (현재는 테스트를 위해 임시로 flow 를 사용)
         val progressRing = findViewById<Timer>(R.id.timer)
@@ -28,5 +36,18 @@ class MainActivity : AppCompatActivity() {
                 progressRing.setCurrentTime(percent) // 현재 시간 적용
             }
         }
+    }
+
+    private fun testTimerWorkerRequest() {
+        TimerThread(
+            context = this,
+            startTime = 0,
+            endTime = 3000,
+        ) {}.run()
+        Log.e(TAG_TIMER_THREAD, "타이머 시작")
+    }
+
+    companion object {
+        const val TAG_TIMER_THREAD = "TAG_TIMER_THREAD"
     }
 }
