fc9cea1dfe39cc5877622d50d745bfb22fdb58dd
nosorae <nosorae@naver.com>
2022-02-27 13:11:48 +0900

    Feat(TimerWorker.kt): 테스트를 위해 Worker 에 임시로 Toast, postDelayed 추가 (후에 Notification 으로 변환 예정)

--- Timer/src/main/java/com/partnus/timer/back/TimerWorker.kt
+++ Timer/src/main/java/com/partnus/timer/back/TimerWorker.kt
@@ -4,16 +4,25 @@ import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import androidx.core.os.postDelayed
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class TimerWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
-        showToast()
-        return Result.success()
+        try {
+            inputData.getString(TimerHandler.WORKER_DATA_PARAM_TIME)?.toLong()?.let { ms ->
+                showToastDelayed(ms) // TODO Toast 를 Notification 코드로 바꾸기
+            }
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+
+
     }
-    private fun showToast() {
-        Handler(Looper.getMainLooper()).post {
+    private fun showToastDelayed(ms: Long) {
+        Handler(Looper.getMainLooper()).postDelayed(ms) {
             Toast.makeText(applicationContext, "TimerWorker 호출됨", Toast.LENGTH_LONG).show()
         }
     }
